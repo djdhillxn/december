cd55475cb5afc0e69c88c1c267cd7bd7a424270c
Dheeraj Dhillon <dhillondheeraj84@gmail.com>
2023-12-27 04:56:15 +0530

    added info script

---
+++ info.py
@@ -0,0 +1,5 @@
+from model import HangmanGRUNet
+model = HangmanGRUNet(hidden_dim=512, gru_layers=2) # replace with your actual model and parameters
+num_parameters = sum(p.numel() for p in model.parameters() if p.requires_grad)
+print(f"Number of trainable parameters: {num_parameters}")
+
